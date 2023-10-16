328dbbf7d31938d9e77f520e28dc2f2e5431f1ff
unknown <BT_2N2_07@bktech.local>
2023-10-16 13:24:55 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,19 @@
+public class LinearEquation {
+    private String coord1;
+    private String coord2;
+    private int coord1X;
+    private int coord1Y;
+    private int coord2X;
+    private int coord2Y;
+    public LinearEquation(String coord1, String coord2) {
+        this.coord1 = coord1;
+        this.coord2 = coord2;
+    }
+
+    public void parseCoords() {
+
+    }
+
+
+
+}
---
+++ src/LinearEquationLogic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class LinearEquationLogic {
+    private Scanner scan;
+
+    public LinearEquationLogic() {
+        scan = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.print("enter coord 1: ");
+        String coord1 = scan.nextLine();
+        System.out.print("enter coord 2: ");
+        String coord2 = scan.nextLine();
+
+    }
+
+
+
+
+
+}
