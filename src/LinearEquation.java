b21a7b61ae3f7fe2d98b976cdc937eb4ffc4e667
sample <sample@bths.edu>
2023-10-19 13:55:12 -0400

    can I copy your code?

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,8 +5,7 @@ public class LinearEquation {
     private int coord1Y;
     private int coord2X;
     private int coord2Y;
-    private double actualSlope;
-    private String slope;
+    private double slope;
     public LinearEquation(int coord1X, int coord1Y, int coord2X, int coord2Y) {
         this.coord1X = coord1X;
         this.coord1Y = coord1Y;
@@ -30,25 +29,39 @@ public class LinearEquation {
         return coord2Y;
     }
 
-    public String getSlope() {
+    public double getSlope() {
         return slope;
     }
 
 
 
-    public void calcSlope(int coord1X, int coord1Y, int coord2X, int coord2Y) {
-        slope = ((coord2Y - coord1Y) + "/" + (coord2X - coord1X));
-        actualSlope = (double) ((coord2Y - coord1Y) / (coord2X - coord1X));
+    public double slope() {
+        slope = ((double) coord2Y - coord1Y) / (coord2X - coord1X);
+        return Math.round(slope * 100) / 100.0;   }
+
+    public String equation() {
+        if (coord1Y == coord2Y) {
+            return "y = " + yIntercept(coord1X, coord1Y);
+        }
+        String equation = ((coord2Y - coord1Y) + "/" + (coord2X - coord1X));
+        double intercept = yIntercept(coord1X, coord1Y);
+        return "y = " + equation + "x + " + intercept;
     }
 
-    public double getYIntercept(int coord1X, int coord1Y) {
-        return (double) coord1Y /  (coord1X * actualSlope);
+    public double yIntercept(int coord1X, int coord1Y) {
+        return (double) coord1Y /  (coord1X * slope);
     }
 
     private double distance() {
         return Math.sqrt(Math.pow(coord2X - coord1X, 2) + Math.pow(coord2Y - coord1Y, 2) );
     }
 
+    private String coordinateforX(double x) {
+        return "(" + x + ", " + (slope * x + yIntercept(coord1X, coord1Y)) + ")";
+    }
 
+    private String lineInfo() {
+        return "The two points are: (" + coord1X + ", " + coord1Y + ") and (" + coord2X + ", " + coord2Y + ")\nThe equation of the line between these points is: " + equation() + "\n The slope of this line is " + slope + "The y-intercept of this line is: " + yIntercept(coord1X, coord1Y) + "\nThe distance between these points is: " + distance();
+    }
 
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -15,39 +15,6 @@ public class LinearEquationLogic {
         scan = new Scanner(System.in);
     }
 
-    public void start() {
-        System.out.print("enter coord 1: ");
-        coord1 = scan.nextLine();
-        System.out.print("enter coord 2: ");
-        coord2 = scan.nextLine();
-        parseCoordinates(coord1, coord2);
-        setSlope();
-        printSlope();
-    }
-
-    private void parseCoordinates(String coord1, String coord2) {
-        int commaIndex = coord1.indexOf(",");
-        coord1X = Integer.parseInt(coord1.substring(1, commaIndex));
-        coord1Y = Integer.parseInt(coord1.substring(commaIndex + 2, coord1.length() - 1));
-        commaIndex = coord2.indexOf(",");
-        coord2X = Integer.parseInt(coord2.substring(1, commaIndex));
-        coord2Y = Integer.parseInt(coord2.substring(commaIndex + 2, coord2.length() - 1));
-        linearEquation = new LinearEquation(coord1X, coord1Y, coord2X, coord2Y);
-    }
-
-    private void setSlope() {
-        linearEquation.calcSlope(coord1X, coord1Y, coord2X, coord2Y);
-        slope = linearEquation.getSlope();
-        yIntercept = linearEquation.getYIntercept(coord1X, coord1Y);
-    }
-
-    private void printSlope() {
-        System.out.println("Slope is y = " + slope + "x + " + yIntercept);
-    }
-
-    private double rounddedToHundredth(double toRound) {
-        return (Math.round(toRound) * 100.0) / 100;
-    }
 
 
 }
